16959fdb406a4f176e1bb9c12a4f9fc231637117
Yudiel <yhonnierrg@gmail.com>
2023-10-28 20:30:26 -0400

    Initial Commit

---
+++ Ynstagram/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ynstagram
+//
+//  Created by Honnier on 28/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ynstagram/YnstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  YnstagramApp.swift
+//  Ynstagram
+//
+//  Created by Honnier on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct YnstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
